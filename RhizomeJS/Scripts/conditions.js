8e52eca189c706069b722979d5924a57f0c3a7b6
Yuri Martynov <yuri.martynov@gmail.com>
2013-05-31 16:03:23 +0300

    Conditions.Random

--- RhizomeJS/Scripts/conditions.js
+++ RhizomeJS/Scripts/conditions.js
@@ -2,17 +2,17 @@
 
 }
 
-Conditions.Any = function () {
+Conditions.Any = function() {
     var states = arguments;
 
     if (states.length == 0)
-        throw "no target state(s) in arguments";
+        throw new Error("no target state(s) in arguments");
 
     if (states.length == 1) {
         return states[0];
     }
 
-    return function () {
+    return function() {
         var i = Math.floor((Math.random() * states.length - 1) + 1);
         return states[i];
     };
@@ -22,7 +22,7 @@ Conditions.Row = function() {
     var states = arguments;
 
     if (states.length == 0)
-        throw "no target state(s) in arguments";
+        throw new Error("no target state(s) in arguments");
 
     if (states.length == 1) {
         return states[0];
@@ -31,12 +31,43 @@ Conditions.Row = function() {
     function row() {
         var index = 0;
 
-        this.transition = function () {
+        this.transition = function() {
             var oldIndex = index;
             index = (index + 1) % states.length;
             return states[oldIndex];
         };
-    };
+    }
+
+    ;
 
     return new row().transition;
-}
+};
+
+Conditions.Random = function () {
+    var states = arguments;
+
+    if (states.length == 0)
+        throw new Error("no target state(s) in arguments");
+
+    if (states.length == 1) {
+        return states[0].state;
+    }
+
+    var sum = 0;
+    for (var i = 0; i < states.length; i++) {
+        sum += states[i].prob;
+    }
+
+    return function() {
+        var rnd = Math.random() * sum;
+        var s = 0;
+        
+        for (var j = 0; j < states.length; j++) {
+            var state = states[j];
+            s += state.prob;
+            if (rnd <= s) return state.state;
+        }
+
+        throw Error("Conditions.Random internal error");
+    };
+};
